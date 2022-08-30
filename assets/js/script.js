0f527b2e4bd83e824a401329c200f7654c480a25
ShahemNezamEddin <shahem.nezam.eddin@gmail.com>
2022-08-30 13:00:37 +0000

    Add eventListener to roll button to generate and display random dice

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,11 @@ let score1El = document.getElementById("score-1");
 let current0El = document.getElementById("current-0");
 let current1El = document.getElementById("current-1");
 let diceEl = document.querySelector(".dice");
-console.log(diceEl);
-//*When the game lode and if now game button clicked
+let btnNew = document.querySelector(".btn-new");
+let btnRoll = document.querySelector(".btn-roll");
+let btnHold = document.querySelector(".btn-hold");
+
+// When the game lode and if now game button clicked
 
 function newGame(){
     score0El.innerText = 0;
@@ -16,3 +19,14 @@ function newGame(){
     diceEl.classList.add("hidden");   
 }
 newGame();
+
+// Add eventListener to roll button
+btnRoll.addEventListener("click", function(){
+    //Generate random dice roll
+    let dice = Math.floor(Math.random() * 6) + 1;
+    //Display dice roll
+    diceEl.classList.remove("hidden");
+    diceEl.src = `assets/images/dice-${dice}.png`;
+    //Is it a 1? 
+
+});
